ffdbd9d874a07975d5ab49be1e5f634f9672529e
DuyetVu2001 <vungocduyet24@gmail.com>
2021-09-02 23:40:52 +0700

    implement login page, login graphql

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql',
+	cache: new InMemoryCache(),
+	credentials: 'include',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider defaultTheme="system" attribute="class">
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<ApolloProvider client={client}>
+			<ThemeProvider defaultTheme="system" attribute="class">
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</ApolloProvider>
 	);
 }
 export default MyApp;
